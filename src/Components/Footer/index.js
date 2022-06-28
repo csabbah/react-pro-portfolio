b61397ef824fec9f0b19160cac5da9ad670f6dd9
Csabbah <csabbah@ryerson.ca>
2022-06-28 15:53:06 -0400

    Updated About me component and made other CSS changes

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const index = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <img
+        className="footer-icons"
+        src="https://i.imgur.com/Dr5B5zu.png"
+        alt="Icon of the Github logo"
+      />
+    </>
+  );
 };
 
 export default index;
