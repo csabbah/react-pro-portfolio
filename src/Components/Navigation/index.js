5a8417cc49777e9af8a8b93b91ff750a6c9dcc5c
Csabbah <csabbah@ryerson.ca>
2022-06-28 15:25:59 -0400

    Updated Header and Navigation components

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const index = () => {
-  return <div>Navigation</div>;
+  return (
+    <ul>
+      <li>
+        <a href="#">About me</a>
+      </li>
+      <p>/</p>
+      <li>
+        <a href="#">Portfolio</a>
+      </li>
+      <p>/</p>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+      <p>/</p>
+      <li>
+        <a href="#">Resume</a>
+      </li>
+    </ul>
+  );
 };
 
 export default index;
