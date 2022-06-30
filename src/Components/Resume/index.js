4733ac5ad00178cfa3ebc5068365b3b7b708714b
Csabbah <csabbah@ryerson.ca>
2022-06-30 15:39:05 -0400

    Updated the CSS for the Resume component

--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -8,41 +8,43 @@ const Resume = () => {
       <a href={`./Assets/Resume/Carlos'Resume(2022_may).pdf`} download>
         Download my Resume here
       </a>
-      <div id="frontend">
-        <h1>Frontend Proficiencies</h1>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Bootstrap</li>
-          <li>Tailwind CSS</li>
-          <li>DaisyUi</li>
-          <li>jQuery and jQueryUI</li>
-        </ul>
-      </div>
-      <div id="dev-tools">
-        <h1>Dev Tools Proficiencies</h1>
-        <ul>
-          <li>Git</li>
-          <li>npm</li>
-          <li>Jest</li>
-          <li>Webpack</li>
-        </ul>
-      </div>
-      <div id="backend">
-        <h1>Backend Proficiencies</h1>
-        <ul>
-          <li>Express</li>
-          <li>Heroku</li>
-          <li>MongoDB</li>
-          <li>MySQL</li>
-          <li>SQLite</li>
-          <li>IndexedDB</li>
-          <li>LocalStorage</li>
-          <li>Service Worker</li>
-          <li>PWA, OOP, MVC and ORM development</li>
-        </ul>
+      <div id="resume-wrapper">
+        <div id="frontend">
+          <h1>Frontend Proficiencies</h1>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+            <li>Tailwind CSS</li>
+            <li>DaisyUi</li>
+            <li>jQuery and jQueryUI</li>
+          </ul>
+        </div>
+        <div id="backend">
+          <h1>Backend Proficiencies</h1>
+          <ul>
+            <li>Express</li>
+            <li>Heroku</li>
+            <li>MongoDB</li>
+            <li>MySQL</li>
+            <li>SQLite</li>
+            <li>IndexedDB</li>
+            <li>LocalStorage</li>
+            <li>Service Worker</li>
+            <li>PWA, OOP, MVC and ORM development</li>
+          </ul>
+        </div>
+        <div id="dev-tools">
+          <h1>Dev Tools Proficiencies</h1>
+          <ul>
+            <li>Git</li>
+            <li>npm</li>
+            <li>Jest</li>
+            <li>Webpack</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
