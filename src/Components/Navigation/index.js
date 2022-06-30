375f54c88a1c8addb5cb69c4619df6dc5c1f28a8
Csabbah <csabbah@ryerson.ca>
2022-06-30 16:01:10 -0400

    Added new nav icon that displays when screen is under 800px

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -13,47 +13,54 @@ const Navigation = ({ setActivePage, activePage }) => {
     }
   };
   return (
-    <ul>
-      <li>
-        <a
-          className={`navItem ${activePage.about ? 'active' : ''}`}
-          href="#"
-          onClick={(e) => handlePage(e)}
-        >
-          About me
-        </a>
-      </li>
-      <p>/</p>
-      <li>
-        <a
-          className={`navItem ${activePage.portfolio ? 'active' : ''}`}
-          href="#"
-          onClick={(e) => handlePage(e)}
-        >
-          Portfolio
-        </a>
-      </li>
-      <p>/</p>
-      <li>
-        <a
-          className={`navItem ${activePage.contact ? 'active' : ''}`}
-          href="#"
-          onClick={(e) => handlePage(e)}
-        >
-          Contact
-        </a>
-      </li>
-      <p>/</p>
-      <li>
-        <a
-          className={`navItem ${activePage.resume ? 'active' : ''}`}
-          href="#"
-          onClick={(e) => handlePage(e)}
-        >
-          Resume
-        </a>
-      </li>
-    </ul>
+    <div>
+      <ul>
+        <li>
+          <a
+            className={`navItem ${activePage.about ? 'active' : ''}`}
+            href="#About"
+            onClick={(e) => handlePage(e)}
+          >
+            About me
+          </a>
+        </li>
+        <p>/</p>
+        <li>
+          <a
+            className={`navItem ${activePage.portfolio ? 'active' : ''}`}
+            href="#Portfolio"
+            onClick={(e) => handlePage(e)}
+          >
+            Portfolio
+          </a>
+        </li>
+        <p>/</p>
+        <li>
+          <a
+            className={`navItem ${activePage.contact ? 'active' : ''}`}
+            href="#Contact"
+            onClick={(e) => handlePage(e)}
+          >
+            Contact
+          </a>
+        </li>
+        <p>/</p>
+        <li>
+          <a
+            className={`navItem ${activePage.resume ? 'active' : ''}`}
+            href="#Resumes"
+            onClick={(e) => handlePage(e)}
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
+      <img
+        id="nav-stack"
+        src="https://imgpile.com/images/RVqHWF.png"
+        alt="Navigation Icon"
+      ></img>
+    </div>
   );
 };
 
