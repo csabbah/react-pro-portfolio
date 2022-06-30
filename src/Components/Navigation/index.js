ec1b93b9fb68bfe6f5d6f558b9d7fa32f64fa07f
Csabbah <csabbah@ryerson.ca>
2022-06-30 16:52:51 -0400

    Nav menu on mobile/tablet screens is now fully responsive and functional

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navigation = ({ setActivePage, activePage }) => {
   const handlePage = (e) => {
@@ -11,10 +11,14 @@ const Navigation = ({ setActivePage, activePage }) => {
     } else if (e.target.innerText == 'Resume') {
       setActivePage({ resume: true });
     }
+    setActiveNav(false);
   };
+
+  const [activeNav, setActiveNav] = useState(false);
+
   return (
     <div>
-      <ul>
+      <ul id="desktop-nav">
         <li>
           <a
             className={`navItem ${activePage.about ? 'active' : ''}`}
@@ -48,7 +52,7 @@ const Navigation = ({ setActivePage, activePage }) => {
         <li>
           <a
             className={`navItem ${activePage.resume ? 'active' : ''}`}
-            href="#Resumes"
+            href="#Resume"
             onClick={(e) => handlePage(e)}
           >
             Resume
@@ -56,10 +60,49 @@ const Navigation = ({ setActivePage, activePage }) => {
         </li>
       </ul>
       <img
-        id="nav-stack"
+        onClick={() => setActiveNav(!activeNav)}
+        className={`nav-stack ${activeNav ? 'active' : ''}`}
         src="https://imgpile.com/images/RVqHWF.png"
         alt="Navigation Icon"
       ></img>
+      <ul className={`mobile-nav ${activeNav ? 'active' : ''}`}>
+        <li>
+          <a
+            className={`navItem ${activePage.about ? 'active' : ''}`}
+            href="#About"
+            onClick={(e) => handlePage(e)}
+          >
+            About me
+          </a>
+        </li>
+        <li>
+          <a
+            className={`navItem ${activePage.portfolio ? 'active' : ''}`}
+            href="#Portfolio"
+            onClick={(e) => handlePage(e)}
+          >
+            Portfolio
+          </a>
+        </li>
+        <li>
+          <a
+            className={`navItem ${activePage.contact ? 'active' : ''}`}
+            href="#Contact"
+            onClick={(e) => handlePage(e)}
+          >
+            Contact
+          </a>
+        </li>
+        <li>
+          <a
+            className={`navItem ${activePage.resume ? 'active' : ''}`}
+            href="#Resumes"
+            onClick={(e) => handlePage(e)}
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
